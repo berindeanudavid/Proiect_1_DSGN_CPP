33a49a3ce5cee932c2e2aa94fbea81d77b16f5d3
berindeanudavid <berindeanudavid@github.com>
2025-11-16 18:55:36 +0200

    Abstract Factory and Builder, but moving stopped working.

--- program.cpp
+++ program.cpp
@@ -147,28 +147,28 @@ class Character : public Entity{
 
 class Human : public Character{
     public:
+        int length=1;
+        int width=1;
+        char sign='H';
         Human(int arg0_x, int arg1_y, Board* arg2_the_board, int arg3_age){
             x=arg0_x;
             y=arg1_y;
-            length=1;
-            width=1;
             the_board_ptr=arg2_the_board;
             age=arg3_age;
-            sign='H';
             moveOnBoardIfOk(x,y,length,width,the_board_ptr,sign);
         }
 };
 
 class Elf : public Character{
     public:
+        int length=3; //elfs are taller and fatter than humans :)
+        int width=2;
+        char sign='E';
         Elf(int arg0_x, int arg1_y, Board* arg2_the_board, int arg3_age){
             x=arg0_x;
             y=arg1_y;
-            length=3; //elfs are taller and fatter than humans :)
-            width=2;
             the_board_ptr=arg2_the_board;
             age=arg3_age;
-            sign='E';
             moveOnBoardIfOk(x,y,length,width,the_board_ptr,sign);
         }
 };
@@ -191,9 +191,9 @@ class BaseDecorator : public Character{
         }
 };
 
-class DoubleSpeed : public BaseDecorator{
+class DoubleSpeedAbility : public BaseDecorator{
     public:
-        DoubleSpeed(Character* character) : BaseDecorator(character){}
+        DoubleSpeedAbility(Character* character) : BaseDecorator(character){}
         void moveUp() override{
             BaseDecorator::moveUp();
             BaseDecorator::moveUp();
@@ -240,6 +240,134 @@ class Group : public Character{
         }
 };
 
+class Building : public Entity{
+    public:
+        int width=0;
+        int length=0;
+        char sign;
+        list<string> parts;
+        void listParts(){
+            cout<<"Building parts:\n";
+            for(string p : parts){
+                cout<<p<<endl;
+            }
+        }
+};
+
+class Builder{
+    public: 
+        Building* building_ptr;
+        virtual void buildWalls(string material){}
+        virtual void buildRoof(string type){}
+        void buildWindows(){
+            building_ptr->parts.push_back("windows");
+        }
+        Building* getBuilding(char arg0_sign, int arg1_x, int arg2_y, Board* arg3_the_board_ptr){
+            building_ptr->sign=arg0_sign;
+            building_ptr->x=arg1_x;
+            building_ptr->y=arg2_y;
+            building_ptr->the_board_ptr=arg3_the_board_ptr;
+            moveOnBoardIfOk(building_ptr->x, building_ptr->y, building_ptr->length, building_ptr->width, building_ptr->the_board_ptr, building_ptr->sign);
+            Building* result = building_ptr;
+            building_ptr = new Building();
+            return result;
+        }
+};
+
+class CastleBuilder : public Builder{
+    public:
+        CastleBuilder(){
+            building_ptr=new Building();
+        }
+        void buildWalls(string material) override{
+            building_ptr->parts.push_back(material+" walls");
+            building_ptr->width+=10;//size distinguishes castles from houses
+            building_ptr->length+=5;
+        }
+        void buildRoof(string type) override{
+            building_ptr->parts.push_back(type+" roof");
+            building_ptr->length+=1;
+        }
+};
+
+class HouseBuilder : public Builder{
+    public:
+        HouseBuilder(){
+            building_ptr=new Building();
+        }
+        void buildWalls(string material) override{
+            building_ptr->parts.push_back(material+" walls");
+            building_ptr->width+=5;//size distinguishes castles from houses
+            building_ptr->length+=3;
+        }
+        void buildRoof(string type) override{
+            building_ptr->parts.push_back(type+" roof");
+            building_ptr->length+=2;//roofs of houses are more pointy, therefore more lengthy
+        }
+};
+
+class AbstractFactory{
+    public:
+        char sign;
+        virtual Building* createCastle(int x, int y, Board* the_board_ptr)=0;
+        virtual Building* createHouse(int x, int y, Board* the_board_ptr)=0;
+};
+
+class EuropeanFactory : public AbstractFactory{
+    public:
+        char sign='$'; //because the euro symbol doesn't work
+        Building* createCastle(int x, int y, Board* the_board_ptr) override{
+            CastleBuilder* castle_builder_ptr=new CastleBuilder();
+            castle_builder_ptr->buildWalls("concrete");
+            castle_builder_ptr->buildRoof("concrete flat");
+            //no windows for european castles <3
+            return castle_builder_ptr->getBuilding(sign,x,y,the_board_ptr);
+        }
+        Building* createHouse(int x, int y, Board* the_board_ptr) override{
+            HouseBuilder* house_builder_ptr=new HouseBuilder();
+            house_builder_ptr->buildWalls("concrete");
+            house_builder_ptr->buildRoof("red");
+            house_builder_ptr->buildWindows();
+            return house_builder_ptr->getBuilding(sign,x,y,the_board_ptr);
+        }
+};
+
+class AfricanFactory : public AbstractFactory{
+    public:
+        char sign='|'; 
+        Building* createCastle(int x, int y, Board* the_board_ptr) override{
+            CastleBuilder* castle_builder_ptr=new CastleBuilder();
+            castle_builder_ptr->buildWalls("dirt");
+            castle_builder_ptr->buildRoof("cane");
+            //no windows for african castles <3
+            return castle_builder_ptr->getBuilding(sign,x,y,the_board_ptr);
+        }
+        Building* createHouse(int x, int y, Board* the_board_ptr) override{
+            HouseBuilder* house_builder_ptr=new HouseBuilder();
+            house_builder_ptr->buildWalls("dirt");
+            //africans are very poor: they don't have roofs or windows: just dirt walls
+            return house_builder_ptr->getBuilding(sign,x,y,the_board_ptr);
+        }
+};
+
+class AsianFactory : public AbstractFactory{
+    public:
+        char sign='^'; 
+        Building* createCastle(int x, int y, Board* the_board_ptr) override{
+            CastleBuilder* castle_builder_ptr=new CastleBuilder();
+            castle_builder_ptr->buildWalls("bamboo");
+            castle_builder_ptr->buildRoof("asian specific bamboo");
+            castle_builder_ptr->buildWindows();
+            return castle_builder_ptr->getBuilding(sign,x,y,the_board_ptr);
+        }
+        Building* createHouse(int x, int y, Board* the_board_ptr) override{
+            HouseBuilder* house_builder_ptr=new HouseBuilder();
+            house_builder_ptr->buildWalls("bamboo");
+            house_builder_ptr->buildRoof("asian specific bamboo");
+            return house_builder_ptr->getBuilding(sign,x,y,the_board_ptr);
+        }
+};
+
 int main(){
     int board_width;
     int board_length;
@@ -250,8 +378,8 @@ int main(){
     Board* board1_ptr=new Board(board_width,board_length);
     Human* human1_ptr=new Human(6,7,board1_ptr,100);
     board1_ptr->render();
-    DoubleSpeed* speedyHuman1_ptr=new DoubleSpeed(human1_ptr);
-    DoubleSpeed* speedyHuman2_ptr=new DoubleSpeed(speedyHuman1_ptr);
+    DoubleSpeedAbility* speedyHuman1_ptr=new DoubleSpeedAbility(human1_ptr);
+    DoubleSpeedAbility* speedyHuman2_ptr=new DoubleSpeedAbility(speedyHuman1_ptr);
     speedyHuman2_ptr->moveUp();
     board1_ptr->render();
     speedyHuman2_ptr->moveLeft();
@@ -271,5 +399,15 @@ int main(){
     board1_ptr->render();
     group1_ptr->moveDown();
     board1_ptr->render();
+    EuropeanFactory* eu_factory_ptr = new EuropeanFactory();
+    Building* european_castle_ptr = eu_factory_ptr->createCastle(80,30,board1_ptr);
+    Building* european_house_ptr = eu_factory_ptr->createHouse(50,20,board1_ptr);
+    board1_ptr->render();
+    AsianFactory* asian_factory_ptr = new AsianFactory();
+    Building* asian_house_ptr = asian_factory_ptr->createHouse(80,70,board1_ptr);
+    AfricanFactory* african_factory_ptr = new AfricanFactory();
+    Building* african_castle_ptr = african_factory_ptr->createCastle(25,50,board1_ptr);
+    board1_ptr->render();
+
     return 0;
 }

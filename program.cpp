c75515e0c44b021cd8c1dabfc85f730cf98ccbdd
berindeanudavid <berindeanudavid@github.com>
2025-11-16 02:54:24 +0200

    debugging segfaults all day and night

--- program.cpp
+++ program.cpp
@@ -38,9 +38,7 @@ bool moveOnBoardIfOk(int x, int y, int height, int width, Board* the_board_ptr, 
     }
     for(int i=x;i<x+height;i++){
         for(int j=y;j<y+width;j++){
-            if(the_board_ptr->reference_board.at(i).at(j)!='_'){
-                the_board_ptr->reference_board.at(i).at(j)=sign;
-            }
+            the_board_ptr->reference_board.at(i).at(j)=sign;
         }
     }
     return true;
@@ -75,38 +73,39 @@ class Character : public Entity{
         Character(){}
         void moveUp(){
             if(moveOnBoardIfOk(x-1,y,height,width,the_board_ptr,sign)==true){
-                removeLine(y,width,x,the_board_ptr,sign);
-                x=-1;
+                removeLine(y,width,x+height-1,the_board_ptr,sign);
+                x=x-1;
             }
         }
         void moveDown(){
             if(moveOnBoardIfOk(x+1,y,height,width,the_board_ptr,sign)==true){
                 removeLine(y,width,x,the_board_ptr,sign);
-                x=+1;
+                x=x+1;
             }   
         }
         void moveLeft(){
             if(moveOnBoardIfOk(x,y-1,height,width,the_board_ptr,sign)==true){
-                removeColumn(x,height,y,the_board_ptr,sign);
-                y=-1;
+                removeColumn(x,height,y+width-1,the_board_ptr,sign);
+                y=y-1;
             }
         }
         void moveRight(){
             if(moveOnBoardIfOk(x,y+1,height,width,the_board_ptr,sign)==true){
                 removeColumn(x,height,y,the_board_ptr,sign);
-                y=+1;
+                y=y+1;
             }
         }
 };
 
 class Human : public Character{
     public:
-        Human(int arg0_x, int arg1_y, Board* arg2_the_board){
+        Human(int arg0_x, int arg1_y, Board* arg2_the_board, int arg3_age){
             x=arg0_x;
             y=arg1_y;
             height=1;
             width=1;
             the_board_ptr=arg2_the_board;
+            age=arg3_age;
             sign='H';
             moveOnBoardIfOk(x,y,height,width,the_board_ptr,sign);
         }
@@ -114,12 +113,13 @@ class Human : public Character{
 
 class Elf : public Character{
     public:
-        Elf(int arg0_x, int arg1_y, Board* arg2_the_board){
+        Elf(int arg0_x, int arg1_y, Board* arg2_the_board, int arg3_age){
             x=arg0_x;
             y=arg1_y;
             height=2; //elfs are taller than humans :)
             width=1;
             the_board_ptr=arg2_the_board;
+            age=arg3_age;
             sign='E';
             moveOnBoardIfOk(x,y,height,width,the_board_ptr,sign);
         }
@@ -129,7 +129,53 @@ class BaseDecorator : public Character{
     public:
         Character* character_ptr;
         BaseDecorator(Character* character) : character_ptr(character){}
+        void moveUp(){
+            return character_ptr->moveUp();
+        }
+        void moveDown(){
+            return character_ptr->moveDown();
+        }
+        void moveLeft(){
+            return character_ptr->moveLeft();
+        }
+        void moveRight(){
+            return character_ptr->moveRight();
+        }
+};
 
+class SpeedDecorator : public BaseDecorator{
+    public:
+        SpeedDecorator(Character* character) : BaseDecorator(character){}
+        void moveUp(){
+            //cout<<BaseDecorator::character_ptr->x<<endl;
+            //cout<<"hey\n";
+            if(moveOnBoardIfOk(x-1,y,height,width,the_board_ptr,sign)==true){
+                removeLine(y,width,x+height-1,the_board_ptr,sign);
+                x=x-1;
+            }
+            BaseDecorator::moveUp();
+        }
+        void moveDown(){
+            if(moveOnBoardIfOk(x+1,y,height,width,the_board_ptr,sign)==true){
+                removeLine(y,width,x,the_board_ptr,sign);
+                x=x+1;
+            } 
+            BaseDecorator::moveDown();
+        }
+        void moveLeft(){
+            if(moveOnBoardIfOk(x,y-1,height,width,the_board_ptr,sign)==true){
+                removeColumn(x,height,y+width-1,the_board_ptr,sign);
+                y=y-1;
+            }
+            BaseDecorator::moveLeft();
+        }
+        void moveRight(){
+            if(moveOnBoardIfOk(x,y+1,height,width,the_board_ptr,sign)==true){
+                removeColumn(x,height,y,the_board_ptr,sign);
+                y=y+1;
+            }
+            BaseDecorator::moveRight();
+        }
 };
 
 int main(){
@@ -141,7 +187,14 @@ int main(){
     cin >> board_length;
     Board board1(board_width,board_length);
     Board* board1_ptr=&board1;
-    // Entity e1(2,2,2,3,board1_ptr);
-    board1.render();
+    Human human1=Human(4,5,board1_ptr,100);
+    Human* human1_ptr=&human1;
+    board1_ptr->render();
+    BaseDecorator decoratableHuman1=BaseDecorator(human1_ptr);
+    BaseDecorator* decoratableHuman1_ptr=&decoratableHuman1;
+    SpeedDecorator speedyHuman1=SpeedDecorator(decoratableHuman1_ptr);
+    SpeedDecorator* speedyHuman1_ptr=&speedyHuman1;
+    speedyHuman1_ptr->moveUp();
+    board1_ptr->render();
     return 0;
 }
